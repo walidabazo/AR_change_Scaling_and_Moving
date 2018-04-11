7d076cb1b0f2220e2eb4d0e691413d105f1249e3
Walid Abd El Azeem <walid.abdelazeem@gmail.com>
2018-04-11 14:21:24 +0200

    Add files via upload

---
+++ zoom.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoom : MonoBehaviour {
+
+    protected virtual void LateUpdate()
+    {
+
+       // Lean.LeanTouch.MoveObject(transform, Lean.LeanTouch.DragDelta);
+        // This will scale the current transform based on a multi finger pinch gesture
+        Lean.LeanTouch.ScaleObject(transform, Lean.LeanTouch.PinchScale);
+    }
+}
